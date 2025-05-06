79ab2aac2e9f8bb886bb8487510d82b7b6e80d85
seshanb <125367985+seshanb@users.noreply.github.com>
2025-05-06 19:04:20 +0530

    Update main.tf

--- terraform_modules/main.tf
+++ terraform_modules/main.tf
@@ -1,5 +1,5 @@
 module "rg" {
-  source   = "${path.module}/modules/resource_group"
+  source   = "modules/resource_group"
   name     = var.resource_group_name
   location = var.resource_group_location
 }
