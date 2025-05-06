4c3e4572b23af9f8433592b73d21358d98625ca6
seshanb <125367985+seshanb@users.noreply.github.com>
2025-05-06 18:58:40 +0530

    Create var.tf

---
+++ terraform_modules/var.tf
@@ -0,0 +1,10 @@
+variable "resource_group_name" {
+  type        = string
+  description = "The name of the resource group"
+}
+
+variable "resource_group_location" {
+  type        = string
+  description = "The Azure location"
+  default     = "East US"
+}
