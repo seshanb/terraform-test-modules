fc6377a90e0f2b980851f789753e8023de9d8e68
seshanb <125367985+seshanb@users.noreply.github.com>
2025-05-09 10:15:42 +0530

    Update main.tf

--- terraform_modules/templates/main.tf
+++ terraform_modules/templates/main.tf
@@ -1,5 +1,5 @@
 module "rg" {
   source   = "../modules/resource_group"
-  name     = var.resource_group_name
-  location = var.resource_group_location
+  name     = "testing"
+  location = "East US"
 }
